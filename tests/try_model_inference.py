9df10a80c705134e7c597451b9d015923bc274a6
Jiawei <jiawhe@paypal.com>
2020-04-21 09:55:01 +0800

    add try inference

---
+++ tests/try_model_inference.py
@@ -0,0 +1,53 @@
+# coding=utf-8
+
+import io
+import pickle
+import sys
+
+import requests
+import torch
+import torch.multiprocessing as mp
+from PIL import Image
+from deepcat.models.ImageVecExtractor import ImageVecExtractor
+from deepcat.models.MultiModalClassifier import MultiModalClassifer
+
+from tools.mask_text_spotter_util import MaskTextSpotter
+
+if __name__ == '__main__':
+    DATAPATH = sys
+    # nsfw_model_path = '%s/models/NSFW/nsfw.299x299.h5' % DATAPATH
+    # nsfw_model = NSFWModel(None)
+    # nsfw_model.nsfw_model = nm
+    # print('nsfw_model', nsfw_model)
+    device = torch.device("cpu")
+    print(device)
+    # print('initing ImageVecExtractor ')
+    # ive = ImageVecExtractor(device)
+    # print('initing multimodal')
+    # multimodal_classifier = MultiModalClassifer(model_path='./')
+    # multimodal_classifier.load_model('%s/models/MultiModal/multimodal_newrules_20200225.mod.inst' % DATAPATH,
+    #                                  location_device=device)
+    # print(multimodal_classifier)
+
+    from maskrcnn_benchmark.config import cfg
+
+    # image seeker
+
+    # cfg.merge_from_file('%s/models/OCR/batch.yaml' % DATAPATH)
+    cfg.merge_from_file('%s/models/OCR/batch.yaml' % DATAPATH)
+    # print('cfg', cfg)
+    print('initing ocr model')
+    mts = MaskTextSpotter(
+        cfg,
+        min_image_size=800,
+        confidence_threshold=0.7,
+        output_polygon=True
+    )
+    pickle.dump(mts, open('./masktextspotter.pkl', 'wb'))
+    img_obj = Image.open(io.BytesIO(
+        requests.get('http://www.cbdwarehouseusa.com/wp-content/uploads/2018/05/choicepowder-300x300.jpg').content))
+    res = mts.run_on_pillow_image(img_obj)
+    print(res)
+    new_mts = pickle.load(open('./masktextspotter.pkl', 'rb'))
+    new_res = mts.run_on_pillow_image(img_obj)
+    print(new_res)
