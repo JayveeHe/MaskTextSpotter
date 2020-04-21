e78009f8acf7a7e5524f2ca98777f330d7d9f3a1
Jiawei <jiawhe@paypal.com>
2020-04-22 01:09:36 +0800

    update for text spotter

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ def get_extensions():
 
 setup(
     name="maskrcnn_benchmark",
-    version="0.1.1",
+    version="0.1.3",
     author="fmassa",
     url="https://github.com/facebookresearch/maskrnn-benchmark",
     description="object detection in pytorch",
