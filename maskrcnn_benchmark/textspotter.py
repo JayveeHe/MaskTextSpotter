b9d2524a2ffbae9c6e30bcce0a595c7bc139bb26
Jiawei <jiawhe@paypal.com>
2020-04-22 12:47:09 +0800

    update for text spotter

--- maskrcnn_benchmark/textspotter.py
+++ maskrcnn_benchmark/textspotter.py
@@ -12,12 +12,10 @@ from torchvision import transforms as T
 from maskrcnn_benchmark.modeling.detector import build_detection_model
 from maskrcnn_benchmark.utils.checkpoint import DetectronCheckpointer
 from maskrcnn_benchmark.structures.image_list import to_image_list
-from maskrcnn_benchmark.config import cfg
 from maskrcnn_benchmark.utils.chars import getstr_grid, get_tight_rect
 
 from PIL import Image
 import numpy as np
-import argparse
 
 
 class MaskTextSpotter(object):
